d375eaa0c89e74c5b90c21ccf97f696aa3c15e99
bell77m <75911503+bell77m@users.noreply.github.com>
2026-01-14 16:36:50 +0000

    update

--- src/routes/officer/setting-account/+page.svelte
+++ src/routes/officer/setting-account/+page.svelte
@@ -802,39 +802,7 @@
         </button>
       </div>
     {/if}
-  </main         {:else}
-                  <svg
-                    style="flex-shrink: 0;"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
-                    <polyline points="22 4 12 14.01 9 11.01"></polyline>
-                  </svg>
-                {/if}
-                <span>{message}</span>
-              </div>
-            {/if}
-
-            <button
-              class="primary-btn"
-              type="button"
-              on:click={handleSaveChanges}
-              disabled={!isFormDirty || isSaving}
-            >
-              {isSaving ? "SAVING..." : "SAVE CHANGES"}
-            </button>
-          </div>
-        {/if}
-      </div>
-    </div>
-  </div>
+  </main>
 </div>
 
 <style>
