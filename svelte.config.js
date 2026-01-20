dea1b1ec8e4502c446d6ac74bd51b646228797a5
bell77m <bell77m@gmail.com>
2026-01-20 15:51:39 +0700

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-node';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: preprocess(),
 	kit: {
 		adapter: adapter()
 	}
