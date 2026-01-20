ced5cd4fbc9a8403b03bf219570d7d72eb42d7a1
bell77m <bell77m@gmail.com>
2026-01-20 19:40:54 +0700

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
-import adapter from '@sveltejs/adapter-node'; 
+// svelte.config.js
+import adapter from '@sveltejs/adapter-node'; // <--- ต้องใช้ตัวนี้
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
-    kit: {
-        adapter: adapter() 
-    }
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter() // จะสร้างโฟลเดอร์ build ให้
+	}
 };
 
 export default config;
